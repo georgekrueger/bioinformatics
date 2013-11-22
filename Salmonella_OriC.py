26db7a700300142b6016de23946242c616419469
George Krueger <georgekrueger@gmail.com>
2013-11-21 19:14:02 -0600

    salmonella genome

--- Salmonella_OriC.py
+++ Salmonella_OriC.py
@@ -2,17 +2,18 @@ import Skew
 import Kmers
 
 if __name__ == "__main__":
-    f = open("data\Salmonella_enterica.fasta", "r")
-    f.readline() # skip first line
+    f = open("data\Salmonella_genome.txt", "r")
+    #f.readline() # skip first line
     dna = ""
     for line in f:
         dna += line.rstrip()
     f.close()
     print "done reading file"
     
-    winStart = 3818639
-    winEnd = winStart + 500
-    text = dna[winStart:winEnd]
+    #winStart = 3818639
+    #winEnd = winStart + 500
+    #text = dna[winStart:winEnd]
+    text = dna
     
     k = 9 # kmer size
     d = 1 # num mismatches
